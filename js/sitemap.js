aab51eee0cf40fb410350b289d81b0744fa71ea7
Dulaj Nadawa <dulajnadawa@gmail.com>
2022-03-27 10:34:51 +0530

    init

---
+++ js/sitemap.js
@@ -0,0 +1,31 @@
+document.getElementById('presentation').addEventListener('click',function () {
+   window.location.href = "gallery.html"
+});
+
+document.getElementById('home').addEventListener('click',function () {
+   window.location.href = "main.html"
+});
+
+document.getElementById('products').addEventListener('click',function () {
+   window.location.href = "https://facebook.com"
+});
+
+document.getElementById('quiz').addEventListener('click',function () {
+   window.location.href = "index.html"
+});
+
+document.getElementById('query').addEventListener('click',function () {
+   window.location.href = "index.html"
+});
+
+document.getElementById('gallery').addEventListener('click',function () {
+   window.location.href = "gallery.html"
+});
+
+document.getElementById('about-us').addEventListener('click',function () {
+   window.location.href = "index.html"
+});
+
+document.getElementById('popular').addEventListener('click',function () {
+   window.location.href = "index.html"
+});
