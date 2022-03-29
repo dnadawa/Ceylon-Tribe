67328fdc429d2967f9fda5d7f5c8789d936fba3d
Dulaj Nadawa <dulajnadawa@gmail.com>
2022-03-29 22:57:50 +0530

    Sitemap changed

--- js/sitemap.js
+++ js/sitemap.js
@@ -1,31 +1,47 @@
 document.getElementById('presentation').addEventListener('click',function () {
-   window.location.href = "gallery.html"
+   window.location.href = "";
 });
 
 document.getElementById('home').addEventListener('click',function () {
-   window.location.href = "main.html"
+   window.location.href = "main.html";
 });
 
 document.getElementById('products').addEventListener('click',function () {
-   window.location.href = "https://facebook.com"
+   window.location.href = "";
 });
 
 document.getElementById('quiz').addEventListener('click',function () {
-   window.location.href = "index.html"
+   window.location.href = "";
 });
 
 document.getElementById('query').addEventListener('click',function () {
-   window.location.href = "index.html"
+   window.location.href = "";
 });
 
 document.getElementById('gallery').addEventListener('click',function () {
-   window.location.href = "gallery.html"
+   window.location.href = "gallery.html";
 });
 
 document.getElementById('about-us').addEventListener('click',function () {
-   window.location.href = "index.html"
+   window.location.href = "";
 });
 
 document.getElementById('popular').addEventListener('click',function () {
-   window.location.href = "index.html"
+   window.location.href = "popular-destinations.html";
+});
+
+document.getElementById('loshani').addEventListener('click',function () {
+   window.location.href = "";
+});
+
+document.getElementById('sanjula').addEventListener('click',function () {
+   window.location.href = "";
+});
+
+document.getElementById('ashiru').addEventListener('click',function () {
+   window.location.href = "";
+});
+
+document.getElementById('dulaj').addEventListener('click',function () {
+   window.location.href = "page-editor.html";
 });
